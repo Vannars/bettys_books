cc8c94ee6806f7eb64c0773a2e783146197b99cf
Vannars <151542822+Vannars@users.noreply.github.com>
2024-10-08 23:28:00 +0100

    Lab 2 Login
    /list route function selects users from the database
    login ejs made
    
    /login route
    /loggedin post request (db query compares hashed password against user input - taken from the req body)
    
    login ejs made
    userlist ejs made

--- routes/main.js
+++ routes/main.js
@@ -8,6 +8,7 @@ router.get('/',function(req, res, next){
 })
 
 router.get('/about',function(req, res, next){
+    
     res.render('about.ejs')
 })
 
--- routes/users.js
+++ routes/users.js
@@ -8,6 +8,50 @@ router.get('/register', function (req, res, next) {
     res.render('register.ejs')                                                               
 })    
 
+// New route to handle login
+router.get('/login', function (req, res, next) {
+    // Render the login.ejs view
+    res.render('login.ejs')                                                               
+})
+// router to handle post request for login
+router.post('/loggedin', function (req, res, next) {
+    // Query the database to get the hashed password from the users table
+    let sqlquery = "SELECT hashedPassword FROM users WHERE username = ?"
+    // Execute the SQL query taking the username from the request body
+    db.query(sqlquery, [req.body.username], function(err, result) {
+        // Simple error handling
+        if (err) return next(err);
+        // If no results are returned, the user is not found
+        if (result.length == 0) {
+            res.send('user not found');
+        } else {
+            // use bcrypt taking the body password and the password from the result (index 0) from the query
+            bcrypt.compare(req.body.password, result[0].hashedPassword, function(err, result) {
+                // conditional output based on the result of the comparison
+                if (err) {
+                    res.send ('Login failed');
+                } else if (result) {
+                    res.send('Login successful');
+                } else {
+                    res.send('Login failed');
+                }
+            });
+        }
+    });                                                             
+});
+
+router.get('/list', function(req, res, next){
+    let sqlquery = "SELECT * FROM users" // query database to get all the users
+    // execute sql query
+    db.query(sqlquery, (err, result) => {
+        if (err) {
+            next(err)
+        }
+        
+        res.render("userlist.ejs", {availableUsers:result})
+     })
+})
+
 router.post('/registered', function (req, res, next) {
     const saltRounds = 10;
     const plainPassword = req.body.password
