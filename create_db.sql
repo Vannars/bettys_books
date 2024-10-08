f79c2094370a3c72a25281c85e02622fdfd4fff1
Vannars <151542822+Vannars@users.noreply.github.com>
2024-10-08 21:31:03 +0100

    Lab 2 Databases and the web
    - Bcrypt installed into local machine
    
    - users table made in database
    - Password hashed in users using bcrypt
    - register page updated with password and username
    sql query inserts user into users table and returns a response (including the hashed password for testing purposes)
    
    -

--- create_db.sql
+++ create_db.sql
@@ -6,7 +6,7 @@ USE bettys_books;
 
 # Create the tables
 CREATE TABLE IF NOT EXISTS books (id INT AUTO_INCREMENT,name VARCHAR(50),price DECIMAL(5, 2) unsigned,PRIMARY KEY(id));
-
+CREATE TABLE IF NOT EXISTS users (username VARCHAR(50), firstname VARCHAR(50), lastname VARCHAR(50), email VARCHAR(50), hashedPassword VARCHAR(255));
 # Create the app user
 CREATE USER IF NOT EXISTS 'bettys_books_app'@'localhost' IDENTIFIED BY 'qwertyuiop'; 
 GRANT ALL PRIVILEGES ON bettys_books.* TO ' bettys_books_app'@'localhost';
--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,41 @@
 // Create a new router
 const express = require("express")
 const router = express.Router()
+const bcrypt = require('bcrypt')
+
 
 router.get('/register', function (req, res, next) {
     res.render('register.ejs')                                                               
 })    
 
 router.post('/registered', function (req, res, next) {
-    // saving data in database
-    res.send(' Hello '+ req.body.first + ' '+ req.body.last +' you are now registered!  We will send an email to you at ' + req.body.email)                                                                           
-})
+    const saltRounds = 10;
+    const plainPassword = req.body.password
+
+     // Hash the password
+     bcrypt.hash(plainPassword, saltRounds, function(err, hashedPassword) {
+        if (err) {
+            console.error(err);
+            return next(err);
+        }
+
+        console.log('Hashed Password:', hashedPassword);
+
+        // Store hashed password in your database.
+        let sqlquery = "INSERT INTO users (username, firstname, lastname, email, hashedPassword) VALUES (?,?,?,?,?)";
+
+        // Execute SQL query
+        db.query(sqlquery, [req.body.username, req.body.first, req.body.last, req.body.email, hashedPassword], function(err, result) {
+            if (err) return next(err);
+            console.log("1 record inserted");
+            // Send a response
+            result = 'Hello ' + req.body.first + ' ' + req.body.last + ' you are now registered! We will send an email to you at ' + req.body.email;
+            result += ' Your password is: ' + req.body.password + ' and your hashed password is: ' + hashedPassword;
+            res.send(result);
+        });
+     });
+    
+});
 
 // Export the router object so index.js can access it
 module.exports = router
