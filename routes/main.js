08779da1a02a87624fbcb33527b6f708a508af2d
Vannars <151542822+Vannars@users.noreply.github.com>
2024-10-28 19:19:19 +0000

    LAB 4 A
    Made a weather extension which talks to a web api  and gets real time weather updates .
    
    weather.ejs
    weather_results.ejs
    main.js - get route for londonnow

--- routes/main.js
+++ routes/main.js
@@ -1,7 +1,8 @@
 // Create a new router
-const express = require("express")
-const router = express.Router()
-const {redirectLogin} = require('./redirectlogin')
+const express = require("express") // require the express module
+const router = express.Router()  // create a new router object
+const {redirectLogin} = require('./redirectlogin') // make a cheeky redirectLogin function from the imported redirectlogin.js file
+const request = require("request"); // require the request module
 
 // Handle our routes
 router.get('/', function(req, res, next){
@@ -12,6 +13,44 @@ router.get('/about', function(req, res, next){
     
     res.render('about.ejs')
 })
+
+router.get('/weather', function(req, res, next){
+    res.render('weather.ejs')
+})
+// Route to get weather data
+router.get('/londonnow', (req, res, next) => {
+    // User activates get route in the weather.ejs file
+    // API key, city and URL - url parses key and city into its own string       
+     let city = req.query.search_text; // Query from weather.ejs is used for city (search query from the weather.ejs page)
+    let apiKey = '93ba31901386c85d2efea1fca67e2d59';
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+
+    // Request the data from the API using the URL
+    request(url, function (err, response, body) {
+        if (err) {
+            next(err);
+        } else {
+            //Define Js object with the parsed JSON data
+              var weather = JSON.parse(body);
+            // If the weather object is not undefined and the main object is not undefined
+              if(weather!==undefined && weather.main!==undefined) {
+                // Render weather_result.ejs - include the followng JSON data as variables
+                res.render('weather_result.ejs', {
+                     region: weather.sys.country,
+                     city: weather.name, 
+                     temperature: weather.main.temp, 
+                     feels_like: + weather.main.feels_like, 
+                     humidity: weather.main.humidity,  
+                     description: weather.weather[0].description
+                     //^^These will be used in weather_result.ejs^^
+              }); 
+              //Error handle for no results found (e.g cities that do not exist)
+            } else {
+                res.send('No results found for: ' + req.query.search_text);
+              };
+        };
+    });
+});
 
 // Export the router object so index.js can access it
 module.exports = router
